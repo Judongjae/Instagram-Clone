5dd72db19d381079452f6f6b0a046e3921dda916
KimGiTae <rlxo6919@naver.com>
2021-10-18 20:16:01 +0900

    auth 중복 제거

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,17 @@
-const express = require("express");
-const { authTest, auth, register, chkLogin } = require("../controller/auth");
+const express = require('express');
+
 const router = express.Router();
-const { authTest, auth, register, chkLogin } = require("../controller/auth");
+const { authTest, auth, register, chkLogin } = require('../controller/auth');
 //이걸 왜 또 쓰신걸까?
-const authMiddleware = require("../auth-middleware/auth");
-router.get("/hi", authMiddleware, authTest);
-router.post("/register", register);
-router.post("/auth", auth);
-router.get("/chkLogin", authMiddleware, chkLogin);
+const authMiddleware = require('../auth-middleware/auth');
+router.get('/hi', authMiddleware, authTest);
+router.post('/register', register);
+router.post('/auth', auth);
+router.get('/chkLogin', authMiddleware, chkLogin);
 
-const { authTest, auth, register, chkLogin } = require("../controller/auth");
-const authMiddleware = require("../auth-middleware/auth");
-router.get("/hi", authMiddleware, authTest);
-router.post("/register", register);
-router.post("/auth", auth);
-router.get("/chkLogin", authMiddleware, chkLogin);
+router.get('/replyList/:postID');
 
-router.get("/replyList/:postID");
+router.post('/replyPost');
 
-router.post("/replyPost");
-
-router.patch("/replyDelete");
+router.patch('/replyDelete');
 module.exports = router;
