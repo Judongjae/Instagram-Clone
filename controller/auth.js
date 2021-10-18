4df5cc44ad05d3f202a02ac86927f0a23b8c509f
fnrkp089 <fnrkp089@gmail.com>
2021-10-18 15:10:00 +0900

    dotenv require

--- controller/auth.js
+++ controller/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/auth');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const authTest = async (req, res) => {
   res.send('hi');
