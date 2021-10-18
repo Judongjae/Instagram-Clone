bcf4f1e5329880c205bb5907572e8e15df6d273f
KimGiTae <rlxo6919@naver.com>
2021-10-18 17:10:59 +0900

    userImageUrl추가

--- routes/post.js
+++ routes/post.js
@@ -7,15 +7,16 @@ const {
   postModify,
   postDelete,
 } = require('../controller/post');
+const authMiddleware = require('../auth-middleware/auth');
 //모든 게시글 불러오기
 router.get('/postList', postList);
 //상세 게시글 조회하기
 router.get('/inspect/:postID', postDetail);
 //게시글 등록하기
-router.post('/posting', posting);
+router.post('/posting', authMiddleware, posting);
 //게시글 수정
-router.patch('/postModify', postModify);
+router.patch('/postModify', authMiddleware, postModify);
 //게시글 삭제
-router.patch('/postDelete', postDelete);
+router.patch('/postDelete', authMiddleware, postDelete);
 
 module.exports = router;
