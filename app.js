c4a4af17ba911702ac18ceafe37f00cfeef97c21
KimGiTae <rlxo6919@naver.com>
2021-10-18 17:53:11 +0900

    cors추가

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 const port = 3000;
 require('./dbConnect');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'));
 
 app.use('/', require('./routes/auth'));
 app.use('/post', require('./routes/post'));
